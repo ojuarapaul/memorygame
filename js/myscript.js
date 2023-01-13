d915b2bdb9e4b46a07fd22a6b30202d3827b685f
Paulo <psfaraujo@gmail.com>
2023-01-13 15:59:45 -0800

    new js

--- js/myscript.js
+++ js/myscript.js
@@ -1,3 +1,15 @@
 function goAhead() {
     var t = setTimeout("window.open('game.html', '_self')", 4000);
 }
+
+
+function runGame() { // chooses a random number from 1 to 8 then displays the correspondent image
+    var randomP1 = Math.floor(Math.random() * 8);
+    randomP1 = randomP1 + 1;
+    var img = document.createElement("img");
+    img.src = ("images/" + randomP1 + ".png");
+    var src = document.getElementById("image-1");
+    img.style.width = '40%';
+    img.style.height = 'auto';
+    src.appendChild(img);
+}
