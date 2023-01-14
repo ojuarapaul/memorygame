1be688b1b05719bb21aeb4f06d23218a47532668
Paulo <psfaraujo@gmail.com>
2023-01-13 17:06:20 -0800

    run game implementation

--- js/myscript.js
+++ js/myscript.js
@@ -9,7 +9,34 @@ function runGame() { // chooses a random number from 1 to 8 then displays the co
     var img = document.createElement("img");
     img.src = ("images/" + randomP1 + ".png");
     var src = document.getElementById("image-1");
-    img.style.width = '40%';
-    img.style.height = 'auto';
+    img.style.width = "6rem";
+    img.style.height = "6rem";
+    src.appendChild(img);
+
+    var randomP1 = Math.floor(Math.random() * 8);
+    randomP1 = randomP1 + 1;
+    var img = document.createElement("img");
+    img.src = ("images/" + randomP1 + ".png");
+    var src = document.getElementById("image-2");
+    img.style.width = "6rem";
+    img.style.height = "6rem";
+    src.appendChild(img);
+
+    var randomP1 = Math.floor(Math.random() * 8);
+    randomP1 = randomP1 + 1;
+    var img = document.createElement("img");
+    img.src = ("images/" + randomP1 + ".png");
+    var src = document.getElementById("image-3");
+    img.style.width = "6rem";
+    img.style.height = "6rem";
+    src.appendChild(img);
+
+    var randomP1 = Math.floor(Math.random() * 8);
+    randomP1 = randomP1 + 1;
+    var img = document.createElement("img");
+    img.src = ("images/" + randomP1 + ".png");
+    var src = document.getElementById("image-4");
+    img.style.width = "6rem";
+    img.style.height = "6rem";
     src.appendChild(img);
 }
